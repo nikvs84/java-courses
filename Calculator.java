4406519d220730f6ed750fed757817fa4f59e412
Nikvs84 <nikvs84@rambler.ru>
2016-12-24 23:25:12 +0500

    upgrade Calculator and InteractRunner

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,28 @@
 public class Calculator {
-	private int result;
+	private double result;
 
-	public void add(int... params) {
-		for (Integer param: params) {
+	public void add(double... params) {
+		for (double param: params) {
 		  this.result += param;
 		}
 	}
 
-	public int getResult() {
+	public void sub(double... params) {
+		this.result = params[0] - params[1];
+	}
+
+	public void mult(double... params) {
+		this.result = params[0] * params[1];
+	}	
+
+	public void div(double... params) {
+		if (params[1] != 0) {
+		  this.result = params[0] * params[1];
+		}
+	}	
+
+
+	public double getResult() {
 		return this.result;
 	}
 
--- InteractRunner.java
+++ InteractRunner.java
@@ -7,11 +7,34 @@ public class InteractRunner {
 	 		Calculator calc = new Calculator();
 			String exit = "no";
 			while (!exit.equals("yes")) {
+				System.out.println("Select operation (add (+), sub (-), mult (*), div (/) :)");
+				String operation = reader.next();
 			 	System.out.println("Enter first arg: ");
 				String first = reader.next();
 				System.out.println("Enter second arg: ");
 				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
+				switch (operation.toLowerCase()) {
+				  case "+": 
+				    calc.add(Double.valueOf(first), Double.valueOf(second));
+				    break;
+				  case "-": 
+				    calc.sub(Double.valueOf(first), Double.valueOf(second));
+				    break;
+				  case "*": 
+				    calc.mult(Double.valueOf(first), Double.valueOf(second));
+				    break;
+				  case "/": 
+				    if (second.equals("0")) {
+				      	System.out.println("Division by zero!");
+				    } else {
+				    	calc.div(Double.valueOf(first), Double.valueOf(second));
+				    }
+				    break;
+				  default:
+				    
+				    break;
+				}
+				
 				System.out.println("Result: " + calc.getResult());
 				calc.cleanResult();
 				System.out.println("Exit: yes/no ");
